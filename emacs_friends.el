fa550c5e175e7056daabd8098c49b34c31055622
Ustun Ozgur <ustunozgur@gmail.com>
2014-10-19 18:12:28 +0300

    Support for linux and windows
    not tested yet

--- emacs_friends.el
+++ emacs_friends.el
@@ -1,24 +1,41 @@
 ;;; -*- lexical-binding: t -*-
+
 (setq uo-programs
-      '(
-        ("sublime" . "Sublime Text")
-        ("vim" . "MacVim")
-        ("intellij" . "IntelliJ IDEA 13")
-        ("textedit" . "TextEdit")
-        ("finder" . "Finder")
-        ("chrome" . "Google Chrome")
-        ("firefox" . "Firefox")
-        ("safari" . "Safari")
-        ("mail" . "Mail")
-      ))
+      (cl-case system-type
+        ('darwin
+         '(
+           ("sublime" . "Sublime Text")
+           ("vim" . "MacVim")
+           ("intellij" . "IntelliJ IDEA 13")
+           ("textedit" . "TextEdit")
+           ("finder" . "Finder")
+           ("chrome" . "Google Chrome")
+           ("firefox" . "Firefox")
+           ("safari" . "Safari")
+           ("mail" . "Mail")
+           ))
+        ('windows-nt
+         '(
+           ("sublime" . "sublime.exe")
+           ("vim" . "gvim.exe")))
+        (t
+         '(
+           ("sublime" . "/usr/bin/sublime")
+           ("vim" . "/usr/bin/gvim")))))
+
+
+(defun uo-path-prefix ()
+    (cl-case system-type
+      ('darwin "open -a")
+      (t "")))
 
 (defun uo-generate-functions (program)
   (let ((name (car program))
         (path (cdr program)))
     (fset (intern (format "uo-%s-file" name))
           (lambda () (interactive)
-            (shell-command (format "open -a \"%s\" %s" path buffer-file-name))))
+            (shell-command (format "%s \"%s\" %s" (uo-path-prefix) path buffer-file-name))))
     (fset (intern (format "uo-%s-directory" name))
-          (lambda () (interactive) (shell-command (format "open -a \"%s\" ." path))))))
+          (lambda () (interactive) (shell-command (format "%s \"%s\" ." (uo-path-prefix) path))))))
 
 (mapcar 'uo-generate-functions uo-programs)
